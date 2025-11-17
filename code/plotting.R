90e7420afaa05ee7ebae1885946ed646c9a99041
KaiFrehner <kaifrehner@gmail.com>
2025-11-17 19:59:24 +0100

    fix on plotting, new plots

--- code/plotting.R
+++ code/plotting.R
@@ -34,24 +34,30 @@ weather <- dat_hourly %>%
 weather_filt <- weather %>%
   filter(hour_of_day >= 8, hour_of_day <= 22)
 
+
+
+### Aggregate
+weather_filt_agg <- weather_filt %>%
+  group_by(date, day_of_year, month, year) %>%
+  summarise(
+    daily_rain = sum(precipitation, na.rm = TRUE)  # daily sums 
+  )
+
+
 ### --- 2. MONTHLY MEAN & SD (long-term) ---
 # only use past years for calculations
-current_year <- max(weather_filt$year)
-stats_monthly <- weather_filt %>%
+current_year <- max(weather_filt_agg$year)
+stats_monthly <- weather_filt_agg %>%
   filter(year != current_year) %>%
   group_by(month) %>%
   summarise(
-    mean_val = mean(precipitation, na.rm = TRUE),
-    sd_val   = sd(precipitation, na.rm = TRUE)
+    mean_val = mean(daily_rain, na.rm = TRUE),
+    sd_val   = sd(daily_rain, na.rm = TRUE)
   )
 
 ### --- 3. PLOTS FOR CURRENT YEAR ---
-weather_curr <- weather_filt %>% 
-  filter(year == current_year) %>%
-  group_by(day_of_year, month) %>%
-  summarise(
-    mean_val_d = mean(precipitation, na.rm = TRUE)
-  )
+weather_curr <- weather_filt_agg %>% 
+  filter(year == current_year)
 
 
 # Merge long-term stats
@@ -59,12 +65,12 @@ weather_plot <- weather_curr %>%
   left_join(stats_monthly, by = "month") %>%
   mutate(
     upper_2sd = mean_val + 2 * sd_val,
-    flag_red = mean_val_d > upper_2sd
+    flag_red = daily_rain > upper_2sd,
+    flag_red_fixed = daily_rain > 2.9
   )
 
 # Create one plot per month
 months_list <- unique(weather_plot$month)
-
 
 for(m in months_list){
   
@@ -87,14 +93,14 @@ for(m in months_list){
       y = "density"
     ) +
     theme_minimal()
-  ggsave(paste0("monthly_hist/hist_", "_", m, ".png"), p, width = 7, height = 4)
+  ggsave(paste0("monthly_hist_sum/hist_", "_", m, ".png"), p, width = 7, height = 4)
 }
 
 for(m in months_list) {
   print(m)
   df <- weather_plot %>% filter(month == m)
   
-  p <- ggplot(df, aes(x = day_of_year, y = mean_val_d)) +
+  p <- ggplot(df, aes(x = day_of_year, y = daily_rain)) +
     geom_line(color = "black") +
     geom_point(aes(color = flag_red)) +
     scale_color_manual(values = c("FALSE" = "black", "TRUE" = "red")) +
@@ -108,5 +114,20 @@ for(m in months_list) {
     ) +
     theme_minimal()
   
-  ggsave(paste0("monthly/weather_", current_year, "_", m, ".png"), p, width = 7, height = 4)
+  ggsave(paste0("monthly_sum/weather_", current_year, "_", m, ".png"), p, width = 7, height = 4)
+  
+  p <- ggplot(df, aes(x = day_of_year, y = daily_rain)) +
+    geom_line(color = "black") +
+    geom_point(aes(color = flag_red_fixed)) +
+    scale_color_manual(values = c("FALSE" = "black", "TRUE" = "red")) +
+    geom_hline(aes(yintercept = mean_val), linetype = "solid") +
+    geom_hline(aes(yintercept = 2.9), linetype = "dotted") +
+    labs(
+      title = paste("Weather in", m, current_year),
+      x = "Day of Year",
+      y = "precipitation"
+    ) +
+    theme_minimal()
+  
+  ggsave(paste0("monthly_sum_fixed/weather_", current_year, "_", m, ".png"), p, width = 7, height = 4)
 }
